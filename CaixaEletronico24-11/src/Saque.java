1fbe5ad8e882572a1986b7536cf1a649379e88e2
bruno.coaracy <bruno.coaracy@DSV-BRUNOCOARACY.nectarnet.com.br>
2018-11-26 10:14:21 -0300

    Correção no método saque.

--- CaixaEletronico24-11/src/Saque.java
+++ CaixaEletronico24-11/src/Saque.java
@@ -2,6 +2,9 @@ public class Saque extends Transacao{
 
     public void saque(double valor, Conta conta){
         //this.saldoDisp -= valor;
-        conta.setSaldoDisp(conta.getSaldoDisp() - valor);
+        if(conta.getSaldoTotal()>= valor)
+            conta.setSaldoDisp(conta.getSaldoDisp() - valor);
+        else
+            System.out.println("Saldo indisponível, tente um valor menor.");
     }
 }
